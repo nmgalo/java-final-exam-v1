d758abc9d65113116608d6dda30c0a7791e06d04
Nika <nikolozamgalo@gmail.com>
2021-01-27 19:12:23 +0400

    minor bug fix

--- src/ge/edu/btu/Payment.java
+++ src/ge/edu/btu/Payment.java
@@ -1,17 +1,18 @@
 package ge.edu.btu;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.chainsaw.Main;
 
 public class Payment {
 
-    final static Logger logger = Logger.getLogger(Payment.class);
+    final static Logger logger = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
 
-        Water water = new Water("WT-128712", 2.20);
+        Water water = new Water("WT-128712", 3.20);
         pay(water);
 
-        Electricity electricity = new Electricity("IT-1289", 9.50);
+        Electricity electricity = new Electricity("IT-1289", 12.50);
         pay(electricity);
 
     }
